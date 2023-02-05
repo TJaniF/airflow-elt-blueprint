7afe5810f5e87cabc3e70eb6a33814a33a0eef47
TJaniF <tamara.fingerlin@astronomer.io>
2023-02-05 23:40:37 +0100

    correct minio creds

--- .devcontainer/post_create_script.sh
+++ .devcontainer/post_create_script.sh
@@ -5,11 +5,7 @@ docker tag quay.io/minio/minio:latest minio:latest
 docker run -dt                                  \
   -p 9000:9000 -p 9090:9090                     \
   -v PATH:/mnt/data                             \
-  -v /etc/default/minio:/etc/config.env         \
-  -e "MINIO_CONFIG_ENV_FILE=/etc/config.env"    \
-  -e "MINIO_ROOT_USER=admin"                    \
-  -e "MINIO_ROOT_PASSWORD=admin"                \
   -e "MINIO_VOLUMES=/mnt/data"                  \
-  --name "minio_local_1"                          \
+  --name "minio_container"                          \
   minio server --console-address ":9090"
 astro dev start -n
