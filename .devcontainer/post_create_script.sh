600d3f402ddd85b3979886834237fe1466d19932
TJaniF <tamara.fingerlin@astronomer.io>
2023-02-05 23:19:48 +0100

    script try 2

--- .devcontainer/post_create_script.sh
+++ .devcontainer/post_create_script.sh
@@ -2,14 +2,14 @@
 
 docker pull quay.io/minio/minio
 docker tag quay.io/minio/minio:latest minio:latest
-MINIO_ROOT_USER=admin
-MINIO_ROOT_PASSWORD=admin
-MINIO_VOLUMES="/mnt/data"
 docker run -dt                                  \
   -p 9000:9000 -p 9090:9090                     \
   -v PATH:/mnt/data                             \
   -v /etc/default/minio:/etc/config.env         \
   -e "MINIO_CONFIG_ENV_FILE=/etc/config.env"    \
-  --name "minio_local"                          \
+  -e "MINIO_ROOT_USER=admin"                    \
+  -e "MINIO_ROOT_PASSWORD=admin"                \
+  -e "MINIO_VOLUMES=/mnt/data"                  \
+  --name "minio_local_1"                          \
   minio server --console-address ":9090"
 astro dev start -n
