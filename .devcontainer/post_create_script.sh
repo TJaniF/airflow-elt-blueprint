c30f607634fe9138d6ebb5fe366f9f9ad656920b
TJaniF <tamara.fingerlin@astronomer.io>
2023-02-06 12:24:30 +0100

    docker compose override

--- .devcontainer/post_create_script.sh
+++ .devcontainer/post_create_script.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 
-docker pull quay.io/minio/minio
-docker tag quay.io/minio/minio:latest minio:latest
-docker run -dt                                  \
-  -p 9000:9000 -p 9090:9090                     \
-  -v PATH:/mnt/data                             \
-  -e "MINIO_VOLUMES=/mnt/data"                  \
-  --name "minio_local"                          \
-  minio server --console-address ":9090"
 astro dev start -n --wait 5m
